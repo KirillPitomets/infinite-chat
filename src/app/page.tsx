6094820f1dc2053d8066dc51ba9345ec42d96df0
Kirill <apelsinka220405@gmail.com>
2025-12-28 16:37:30 +0200

    added: Sidebar component, wrote "Welcome message"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Sidebar from "./Components/Sidebar/Sidebar";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
