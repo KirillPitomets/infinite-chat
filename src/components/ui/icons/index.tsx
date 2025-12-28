0e9265dfb0f93362f361008f8c2113e87f9f5a79
Kirill <apelsinka220405@gmail.com>
2025-12-28 17:00:58 +0200

    feat: :lipstick: add icon components (Calendar, camera, message, network, note)

---
+++ src/components/ui/icons/index.tsx
@@ -0,0 +1,6 @@
+import { NetworkIcon } from "./NetworkIcon"
+import { CameraIcon } from './CameraIcon'
+import { MessageIcon } from './MessageIcon'
+import { NoteIcon } from './NoteIcon'
+
+export { NetworkIcon, CameraIcon, MessageIcon, NoteIcon }
