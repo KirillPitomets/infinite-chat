0491ef5b41a84bb46538ccd6652d10799d2461ce
Kirill <apelsinka220405@gmail.com>
2025-12-30 16:41:45 +0200

    feat: :art: make chat header component

--- src/app/(public)/chat/[chatId]/ChatComponion.tsx
+++ src/app/(public)/chat/[chatId]/ChatComponion.tsx
@@ -1,22 +1,23 @@
+import {IconButtonBase} from "@/components/ui/IconButtonBase"
+import {CameraIcon, InformationIcon} from "@/components/ui/icons"
 import Image from "next/image"
 
 interface IProps {
-  img?: string
+  img: string
   name: string
   status: "online" | "offline"
 }
 
-export function ChatComponion({ name, status, img }: IProps) {
-  return <div className="flex gap-2">
-    <div className="max-w-10.5 max-h-10.5 rounded-4xl bg-gray-600 overflow-hidden flex justify-center align-center">
-      <Image width={42} height={42} src="" alt="Companion" />
+export function ChatComponion({name, status, img}: IProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="max-w-10.5 max-h-10.5 rounded-4xl bg-gray-600 overflow-hidden flex justify-center align-center">
+        <Image width={42} height={42} src={img} alt="Companion" />
+      </div>
+      <div>
+        <p className="font-semibold">{name}</p>
+        <span className="text-green-700">{status}</span>
+      </div>
     </div>
-    <div>
-      <p className="font-semibold">
-        {name}
-      </p>
-      <span className="text-green-400">{status}</span>
-    </div>
-  </div>
-
+  )
 }
--- src/app/(public)/chat/[chatId]/ChatHeader.tsx
+++ src/app/(public)/chat/[chatId]/ChatHeader.tsx
@@ -1,8 +1,25 @@
-import { ChatComponion } from "./ChatComponion";
+import {IconButtonBase} from "@/components/ui/IconButtonBase"
+import {ChatComponion} from "./ChatComponion"
+import {CameraIcon, InformationIcon} from "@/components/ui/icons"
 
 export function ChatHeader() {
   return (
-    <header className="border-b border-zinc-300">
+    <header className="flex items-center justify-between p-2.5 border-b border-zinc-300">
+      <ChatComponion
+        name="Travis Barker"
+        status="online"
+        img="https://randomuser.me/api/portraits/men/81.jpg"
+      />
+
+      <div className="flex space-x-1">
+        <IconButtonBase>
+          <CameraIcon />
+        </IconButtonBase>
+
+        <IconButtonBase tone="muted">
+          <InformationIcon />
+        </IconButtonBase>
+      </div>
       
     </header>
   )
