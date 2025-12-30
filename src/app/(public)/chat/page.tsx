341402af49ea27976d3979f3afd6355c1bb1df10
Kirill <apelsinka220405@gmail.com>
2025-12-30 14:04:19 +0200

    style: :lipstick: center content on the page

--- src/app/(public)/chat/page.tsx
+++ src/app/(public)/chat/page.tsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
-    <div className="w-full flex-1">
-      <h1 className="text-4xl text-center">
+    <div className="w-full flex-1 flex items-center justify-center">
+      <h1 className="text-4xl">
         Welcome, to the Infinite chat ❤️😉
       </h1>
     </div>
