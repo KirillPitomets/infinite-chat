1569beb5c4ba3ecb08c86addaf411c1939c00a68
Kirill <apelsinka220405@gmail.com>
2026-01-17 04:27:20 +0200

    feat: :sparkles: init chat scheme and types

---
+++ src/shared/chat.schema.ts
@@ -0,0 +1,82 @@
+import {Prisma} from "@/prisma/generated/client"
+import {Static, t} from "elysia"
+
+const BaseChatDetailsSchema = t.Object({
+  id: t.String(),
+  createdAt: t.String({format: "date-time"}),
+  messages: t.Array(
+    t.Object({
+      id: t.String(),
+      createdAt: t.String({format: "date-time"}),
+      content: t.String(),
+      sender: t.Object({
+        id: t.String(),
+        name: t.String(),
+        tag: t.String()
+      })
+    })
+  )
+})
+
+const DirectChatDetailsSchema = t.Intersect([
+  BaseChatDetailsSchema,
+  t.Object({
+    type: t.Literal("DIRECT"),
+    otherUser: t.Object({
+      id: t.String(),
+      name: t.String(),
+      tag: t.String()
+    })
+  })
+])
+
+const GroupChatDetailsSchema = t.Intersect([
+  BaseChatDetailsSchema,
+  t.Object({
+    type: t.Literal("GROUP"),
+    name: t.String(),
+    membersCount: t.Number()
+  })
+])
+
+export const ChatDetailsSchema = t.Union([
+  DirectChatDetailsSchema,
+  GroupChatDetailsSchema
+])
+
+export type ChatDetailsDTO = Static<typeof ChatDetailsSchema>
+
+export type ChatDetailsPrismaType = Prisma.ChatGetPayload<{
+  select: {
+    id: true
+    type: true
+    name: true
+    createdAt: true
+    messages: {
+      select: {
+        id: true
+        createdAt: true
+        content: true
+        sender: {
+          select: {
+            id: true
+            name: true
+            tag: true
+          }
+        }
+      }
+    }
+    memberships: {
+      select: {
+        role: true
+        user: {
+          select: {
+            id: true
+            name: true
+            tag: true
+          }
+        }
+      }
+    }
+  }
+}>
