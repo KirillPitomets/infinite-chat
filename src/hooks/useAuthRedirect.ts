67958d85b15536371c5c36ec958142644c670b47
Kirill <apelsinka220405@gmail.com>
2026-01-21 02:09:27 +0200

    feat: :sparkles: Implement useAuthRedirect hook

---
+++ src/hooks/useAuthRedirect.ts
@@ -0,0 +1,15 @@
+"use client"
+
+import {ACOOUNT_PAGES} from "@/config/accountPages.config"
+import {useUser} from "@clerk/nextjs"
+import {useRouter} from "next/navigation"
+import {useEffect} from "react"
+
+export const useAuthRedirect = () => {
+  const {user, isSignedIn} = useUser()
+  const {push} = useRouter()
+
+  useEffect(() => {
+    if (user && isSignedIn) push(ACOOUNT_PAGES.HOME)
+  }, [user, isSignedIn, push])
+}
