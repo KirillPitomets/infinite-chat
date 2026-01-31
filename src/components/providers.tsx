a951cb6f8841df78d6cf2ab4d9958763f9644ee8
Kirill <apelsinka220405@gmail.com>
2026-01-31 16:55:55 +0200

    feat: :sparkles: add realtime provider @upstash/realtime

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,12 +3,15 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import {useState} from "react"
 import {ClerkProvider} from "@clerk/nextjs"
+import {RealtimeProvider} from "@upstash/realtime/client"
 
 export const Providers = ({children}: {children: React.ReactNode}) => {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <ClerkProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <RealtimeProvider>{children}</RealtimeProvider>
+      </QueryClientProvider>
     </ClerkProvider>
   )
 }
