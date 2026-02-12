36a6863892ad5e527944f28a0c33af0f273a4225
Kirill <apelsinka220405@gmail.com>
2026-02-12 15:35:54 +0200

    feat: :sparkles: add emojo picker in chat input

--- src/app/account/chat/[chatId]/components/Input.tsx
+++ src/app/account/chat/[chatId]/components/Input.tsx
@@ -1,13 +1,12 @@
 "use client"
 
 import {ChangeEvent, useLayoutEffect, useRef, useState} from "react"
+import EmojiPicker, {EmojiClickData} from "emoji-picker-react"
 
-export function ChatInput({
-  sendFn
-}: {
-  sendFn: (content: string) => void
-}) {
+export function ChatInput({sendFn}: {sendFn: (content: string) => void}) {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const [isOpenEmojiPicker, setIsOpenEmojiPicker] = useState(false)
+  const [latestEmoji, setLatestEmoji] = useState("😀")
   const [value, setValue] = useState<string>("")
 
   const MIN_TEXTAREA_HEIGHT = 32
@@ -47,18 +46,41 @@ export function ChatInput({
     }
   }
 
+  const handleEmojiPicker = () => {
+    setIsOpenEmojiPicker(prev => !prev)
+  }
+
+  const handleEmojiClick = (emoji: EmojiClickData) => {
+    setValue(prev => prev + emoji.emoji)
+    setLatestEmoji(emoji.emoji)
+  }
+
   return (
     <label className="flex items-end border-t border-zinc-300 py-1 pb-5 px-4 space-x-2 relative">
-      <textarea
-        ref={textareaRef}
-        onKeyDown={sendMessageViaEnter}
-        onChange={onChange}
-        value={value}
-        className="w-full max-h-62.5 px-4 py-2.5 text-zinc-900 bg-zinc-50 rounded-2xl transition-colors focus:bg-zinc-200 resize-none"
-        autoFocus
-        rows={1}
-        placeholder="Message..."
-      />
+      <div className="w-full flex items-center bg-zinc-200 rounded-2xl transition-colors focus:bg-zinc-400">
+        <textarea
+          ref={textareaRef}
+          onKeyDown={sendMessageViaEnter}
+          onChange={onChange}
+          value={value}
+          className="w-full max-h-62.5 px-4 py-2.5 text-zinc-900 outline-none resize-none"
+          autoFocus
+          rows={1}
+          placeholder="Message..."
+        />
+
+        <div className="relative">
+          <button className="p-4 cursor-pointer" onClick={handleEmojiPicker}>
+            {latestEmoji}
+          </button>
+          <div className="absolute bottom-full right-0">
+            <EmojiPicker
+              open={isOpenEmojiPicker}
+              onEmojiClick={handleEmojiClick}
+            />
+          </div>
+        </div>
+      </div>
 
       <button
         onClick={sendMessageHandler}
