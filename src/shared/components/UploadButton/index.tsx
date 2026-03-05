72ad24902022af562f52a1ba452b31806e44dde8
Kirill <apelsinka220405@gmail.com>
2026-03-05 13:59:50 +0200

    feat: :lipstick: add button UI and cleanup unused props

--- src/shared/components/UploadButton/index.tsx
+++ src/shared/components/UploadButton/index.tsx
@@ -1,27 +1,24 @@
-import { InputHTMLAttributes } from "react"
 import { ClipIcon, ReloadIcon } from "../ui/icons"
 import { DropzoneInputProps } from "react-dropzone"
+import { IconButtonBase } from "../ui/IconButtonBase"
 
-interface UploadButtonProps extends InputHTMLAttributes<HTMLInputElement> {
+interface UploadButtonProps {
   icon?: "clip" | "reload"
   inputDropZoneProps?: DropzoneInputProps
 }
 
 export const UploadButton = ({
   icon = "clip",
-  accept = "image/*",
-  inputDropZoneProps,
-  ...props
+  inputDropZoneProps
 }: UploadButtonProps) => {
   return (
     <label className="text-green-400 transition-colors cursor-pointer hover:text-green-600">
-      {icon === "clip" ? <ClipIcon /> : <ReloadIcon />}
+      <IconButtonBase>
+        {icon === "clip" ? <ClipIcon /> : <ReloadIcon />}
+      </IconButtonBase>
       <input
         className="absolute opacity-0 -z-1"
         type="file"
-        // accept={accept}
-        // multiple
-        {...props}
         {...inputDropZoneProps}
       />
     </label>
