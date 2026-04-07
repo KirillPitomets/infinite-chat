e318132457bea4d8f65206f9d592f64f007994bb
Kirill <apelsinka220405@gmail.com>
2026-04-08 01:15:47 +0300

    feat: :bug: add creadentials "inclide" in fetch

--- src/shared/lib/eden.ts
+++ src/shared/lib/eden.ts
@@ -1,7 +1,11 @@
 import { treaty } from "@elysiajs/eden"
 import type { App } from "@/app/api/[[...slugs]]/route"
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
 
 // .api to enter /api prefix
-export const edenClient = treaty<App>(BASE_URL || "http://localhost:3000").api
+export const edenClient = treaty<App>(BASE_API_URL, {
+  fetch: {
+    credentials: "include"
+  }
+}).api
