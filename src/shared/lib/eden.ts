cba03ee6cc036d58a716052eaa96ca98a671273c
Kirill <apelsinka220405@gmail.com>
2026-04-08 00:49:23 +0300

    fix: configure edenClient with evnironment-based base URL

--- src/shared/lib/eden.ts
+++ src/shared/lib/eden.ts
@@ -1,6 +1,7 @@
-import {treaty} from "@elysiajs/eden"
-import type {App} from "@/app/api/[[...slugs]]/route"
+import { treaty } from "@elysiajs/eden"
+import type { App } from "@/app/api/[[...slugs]]/route"
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 // .api to enter /api prefix
-export const edenClient = treaty<App>("http://localhost:3000").api
-
+export const edenClient = treaty<App>(BASE_URL || "http://localhost:3000").api
